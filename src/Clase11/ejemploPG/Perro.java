a0640d84dba927120affe1e6883edcc558690153
“nikoam3” <amaya.niko@gmail.com>
2022-08-29 14:26:12 -0300

    Clase 11: practica.

---
+++ src/Clase11/ejemploPG/Perro.java
@@ -0,0 +1,12 @@
+package Clase11.ejemploPG;
+
+public class Perro extends Animal{
+    public Perro(String nombre , Integer edad) {
+        super(nombre , edad);
+    }
+
+    @Override
+    public void hacerRuido() {
+        System.out.println("guau");
+    }
+}
